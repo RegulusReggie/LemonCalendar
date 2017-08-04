29fb06041064d846093c2e1d102c8f343e5ed8f9
Alex23shi <1772618266@qq.com>
2017-08-04 15:26:27 -0700

    07/25 - Added RegInfo and LogInfo for User Access, LocalTips and GroupMember Info 07/25 - Added Users, Groups, and Calendar Entity 07/25 - Added Database Access, database at localhost:3306, root, Passw0rd 07/31 - Added Calendar & Group Factory 08/01 - Modified Calendar Class, Groups Class, CalendarFactory, GroupFactory; added databse connection in Factory classes with functions of creating, add, updating and deleting an entity 08/03 - Modified DBAccess

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,11 +1,16 @@
 package UI;
 
+import Controller.CalendarFactory;
+import Entity.Calendar;
 import Util.DBAccess;
+import Util.Commons;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import static Util.Commons.convertStringToList;
 
 public class Main extends Application {
 
@@ -16,6 +21,9 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
         DBAccess.getDBA();
+        CalendarFactory.insertCal(convertStringToList("1 2 3 4 5 6"), 3, convertStringToList("2 3 4 5 6 7 8"), 5400, 12);
+        //Calendar cal = CalendarFactory.searchCalendar(1);
+        //System.out.println(cal.getEventId());
     }
 
 
